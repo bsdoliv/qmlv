455e0dadf1517e4474f4cf657b1f0fcf46639e0a
Andre de Oliveira <deoliveirambx@googlemail.com>
2015-06-22 10:04:13 -0300

    fix copyright notice on qmlviewer

--- qmlviewer.h
+++ qmlviewer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2015 Andre de Oliveira <deoliveirambx@googlemail.com>
+ * Copyright (c) 2013 Andre de Oliveira <deoliveirambx@googlemail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
